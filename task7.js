de180f45eb7f60b2dbfbaa255eac5de3b1e51407
Marina Moshnogorskaya <marinamoshnogorskaya@gmail.com>
2018-04-18 10:33:12 +0300

    without mobile specifics

--- task7.js
+++ task7.js
@@ -11,19 +11,19 @@ function getMatchingObjects(array, property, value) {
 }
 
 class Project {
-  constructor() {
+  constructor(projectDifficulty) {
     this.projectType = getRandomInt(0, 1) ? "web" : "mobile";
-    this.projectDifficulty = getRandomInt(1, 3);
+    this.projectDifficulty = projectDifficulty; //getRandomInt(1, 3);
   }
 }
 
-
 const director = {
   allProjects: [],
+  projectsDone: 0,
   addProjects() {
     const amount = getRandomInt(0, 4);
     for (let i = 0; i < amount; i++) {
-      this.allProjects.push(new Project());
+      this.allProjects.push(new Project(getRandomInt(1, 3)));
     }
     return this.allProjects;
   },
@@ -47,9 +47,9 @@ const director = {
       } else {
         department.needDevelopers = p.length - freeDevs;
         department.projects = department.projects.concat(
-          p.slice(0, freeDevs)
+          p.slice(0, freeDevs - 1)
         );
-        projectsLeft = p.slice(freeDevs + 1);
+        projectsLeft = p.slice(freeDevs);
         director.allProjects = director.allProjects.concat(projectsLeft);
       }
     }
@@ -68,19 +68,20 @@ const director = {
     hireByDepartment(mobDepartment);
     hireByDepartment(qaDepartment);
   },
-  fired:0,
+  fired: 0,
   fire() {
-    function fireByDepartment(department){
-      for(let i=0; i<department.developers.length;i++){if(department.developers[i].daysFree>3)
-      {department.developers.splice(i,1);
-      director.fired++;}}
+    function fireByDepartment(department) {
+      for (let i = 0; i < department.developers.length; i++) {
+        if (department.developers[i].daysFree > 3) {
+          department.developers.splice(i, 1);
+          director.fired++;
+        }
+      }
     }
     fireByDepartment(webDepartment);
     fireByDepartment(mobDepartment);
     fireByDepartment(qaDepartment);
-    
   }
-  
 };
 
 class Department {
@@ -92,73 +93,99 @@ class Department {
   freeDevelopers() {
     return getMatchingObjects(this.developers, "busyDuration", 0);
   }
-  distributeProjects(){
-   while(this.projects.length!==0){
-   for(let i=0; i<this.developers.length; i++){
-  if(this.developers[i].busyDuration===0){
-  this.developers[i].busyDuration=this.projects[0].projectDifficulty; this.developers[i].daysFree=0;
-  break;
-  }}
-  this.projects.shift();
-}
-    
+  distributeProjects() {
+    while (this.projects.length !== 0) {
+      for (let i = 0; i < this.developers.length; i++) {
+        if (this.developers[i].busyDuration === 0) {
+          this.developers[i].busyDuration = this.projects[0].projectDifficulty;
+          this.developers[i].daysFree = 0;
+          break;
+        }
+      }
+      this.projects.shift();
+    }
   }
-  
-    
 }
 
 let webDepartment = new Department();
 
-webDepartment.workInProgress=function (){
-   for (let i=0; i<this.developers.length; i++){
-   this.developers[i].works();
-   if (this.developers[i].busyDuration===0){qaDepartment.projects++;}
-   }
-  };
-
+webDepartment.workInProgress = function() {
+  for (let i = 0; i < this.developers.length; i++) {
+    this.developers[i].works();
+    if (this.developers[i].busyDuration === 0) {
+      qaDepartment.projectsStack.push(new Project(1));
+    }
+  }
+};
 
 let mobDepartment = new Department();
-mobDepartment.workInProgress=function (){
-   for (let i=0; i<this.developers.length; i++){
-   this.developers[i].works();
-   if (this.developers[i].busyDuration===0){qaDepartment.projects++;}
-   }
-  };
+mobDepartment.workInProgress = function() {
+  for (let i = 0; i < this.developers.length; i++) {
+    this.developers[i].works();
+    if (this.developers[i].busyDuration === 0) {
+      qaDepartment.projectsStack.push(new Project(1));
+    }
+  }
+};
 
-let qaDepartment = new Department();
-qaDepartment.projects=0;
-//mobDepartment.workInProgress=0;
+let qaDepartment = new Department(); //has its own stack of Projects and takeProjects method and modofied workInProgress
+qaDepartment.projectsStack = [];
+qaDepartment.takeProjects = function() {
+  let projectsLeft = [];
+  let freeDevs = this.freeDevelopers().length;
+  if (this.projectsStack.length <= freeDevs) {
+    this.projects = this.projects.concat(this.projectsStack);
+  } else {
+    this.needDevelopers = this.projectsStack.length - freeDevs;
+    this.projects = this.projects.concat(
+      this.projectsStack.slice(0, freeDevs - 1)
+    );
+    projectsLeft = this.projectsStack.slice(freeDevs);
+    this.projects = this.projects.concat(projectsLeft);
+  }
+};
 
+qaDepartment.workInProgress = function() {
+  for (let i = 0; i < this.developers.length; i++) {
+    this.developers[i].works();
+    if (this.developers[i].busyDuration === 0) {
+      director.projectsDone++;
+    }
+  }
+};
 
 class Developer {
   constructor() {
     this.busyDuration = 0;
-    this.daysFree=0;
+    this.daysFree = 0;
   }
-  works(){
-    if (this.busyDuration===0){this.daysFree++;}
-    else{this.busyDuration--; this.daysFree=0;}
-   // if (this.busyDuration===0){/*ready for tests*/}
+  works() {
+    if (this.busyDuration === 0) {
+      this.daysFree++;
+    } else {
+      this.busyDuration--;
+      this.daysFree = 0;
+    }
   }
 }
 
-let days=10;
-for (let k=0; k<days; k++){
-director.hire();
-director.giveProjects();//yesterday's
+let days = 10;
+for (let k = 0; k < days; k++) {
+  director.hire();
+  director.giveProjects(); //yesterday's
 
-director.addProjects();
-director.giveProjects();
-webDepartment.distributeProjects(); //today's
-mobDepartment.distributeProjects();
+  director.addProjects();
+  director.giveProjects(); //today's
+  webDepartment.distributeProjects();
+  mobDepartment.distributeProjects();
 
-webDepartment.workInProgress();
-mobDepartment.workInProgress();
-//testers work on projects
-//delete projects
-director.fire();
+  webDepartment.workInProgress();
+  mobDepartment.workInProgress();
+  qaDepartment.takeProjects();
+  qaDepartment.workInProgress();
+  director.fire();
 }
 
-
-
-console.log(director.fired);
+console.log(
+  director.hired + " " + director.fired + " " + director.projectsDone
+);
